f20ed9490a9c8d8f9df1926ee7acc827f2f47f73
MORRoz <MORRozSpb@gmail.com>
2021-06-08 21:13:00 +0300

    initial updates, basic structure #9

---
+++ trade/src/Login.js
@@ -0,0 +1,14 @@
+ 
+function Login() {
+    return(
+        <div className = 'Login'>
+            <input/>
+            <br/><br/>
+            <input/>
+            <br/><br/>
+            <button>Login</button>
+        </div>
+    );
+}
+
+export default Login;
